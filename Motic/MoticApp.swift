cc035767dada1d31828d9b3061423e65933a7dc6
Ciao Chiang <ciaochiang@gmail.com>
2023-09-14 11:58:46 +0800

    Initial Commit

---
+++ Motic/MoticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoticApp.swift
+//  Motic
+//
+//  Created by Ciao Chiang on 2023/9/14.
+//
+
+import SwiftUI
+
+@main
+struct MoticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
