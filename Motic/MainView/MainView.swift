41f0b98a6035faca0a9d2fd0b8c73d9c61641826
Ciao Chiang <ciaochiang@gmail.com>
2023-09-17 19:49:49 +0800

    enhance main view layout

--- Motic/MainView/MainView.swift
+++ Motic/MainView/MainView.swift
@@ -14,22 +14,19 @@ struct MainView: View {
   var body: some View {
     ZStack {
       ScrollView(showsIndicators: false) {
-        VStack(spacing: 16) {
-          topBar
-          heartRateWidgetCard
-          zoneWidgetCard
-          MainViewSectionHeader(sectionTitle: "Activities")
-          activitiesWidgetCard
+        VStack(spacing: 24) {
+          topSeciton
+          currentActivitySection
+          sumamrySection
+          activitiesSeciton
           Spacer()
-          bottomBar
         }
-        .padding(30)
+        .padding(24)
       }
     }
-    .background(Color.gray.ignoresSafeArea())
-    .onAppear {
-      viewModel.healthStoreManager.retrieveOneMonthActivities()
-    }
+    .background(
+      Color(UIColor.secondarySystemBackground).ignoresSafeArea()
+    )
   }
 }
 
@@ -39,22 +36,79 @@ struct MainView_Previews: PreviewProvider {
   }
 }
 
-// MARK: COMPONENTS
+// MARK: Sections
 extension MainView {
-  var topBar: some View {
+  var topSeciton: some View {
     HStack {
-      Text("Ciao Chiang")
-        .font(.headline)
+      VStack(alignment: .leading, spacing: 8) {
+        Text("Welcome Back")
+          .font(.subheadline)
+          .foregroundColor(.primary)
+        Text("Ciao Chiang")
+          .font(.title2)
+          .fontWeight(.semibold)
+          .foregroundColor(.primary)
+      }
+
       Spacer()
       Image(systemName: "person")
-        .frame(width: 30, height: 30)
-        .foregroundColor(.white)
-        .background(Color.green)
-        .cornerRadius(15)
+        .frame(width: 60, height: 60)
+        .foregroundColor(.black)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color(UIColor(white: 1, alpha: 0.7)), radius: 4)
     }
   }
   
-  var bottomBar: some View {
+  var currentActivitySection: some View {
+    HStack(spacing: 16) {
+      VStack(alignment: .leading, spacing: 8) {
+        Text("Working Out...")
+          .font(.headline)
+          .fontWeight(.bold)
+          .foregroundColor(.white)
+        Text("Indoor Cycling")
+          .font(.subheadline)
+          .fontWeight(.regular)
+          .foregroundColor(.accentColor)
+      }
+      Spacer()
+      VStack(alignment: .center) {
+        Text("1:30:20")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.white)
+      }
+    }
+    .padding(24)
+    .frame(maxWidth: .infinity)
+    .background(Color.black)
+    .cornerRadius(16)
+  }
+  
+  var sumamrySection: some View {
+    VStack(spacing: 8) {
+      MainViewSectionHeader(sectionTitle: "Summary")
+      HStack(spacing: 16) {
+        heartRateWidgetCard
+        zoneWidgetCard
+      }
+    }
+  }
+  
+  var activitiesSeciton: some View {
+    VStack(spacing: 8) {
+      MainViewSectionHeader(sectionTitle: "Activites")
+      
+      VStack(spacing: 16) {
+        ForEach(0..<4) { _ in
+          MainViewActivityCard()
+        }
+      }
+    }
+  }
+  
+  var footerSection: some View {
     HStack {
       Button {
         
@@ -69,31 +123,57 @@ extension MainView {
       .cornerRadius(8)
     }
   }
+}
+
+struct MainViewSectionHeader: View {
+  let sectionTitle: String
   
-  var heartRateWidgetCard: some View {
-    VStack {
-      HStack {
-        Image(systemName: "heart.fill")
-          .foregroundColor(viewModel.isHeartRateAuthoized == .sharingAuthorized ? .pink : .gray)
-        Text("Heart Rate")
-          .fontWeight(.medium)
-          .foregroundColor(viewModel.isHeartRateAuthoized == .sharingAuthorized ? .black : .gray)
-        
-        Spacer()
-        Text(viewModel.isHeartRateAuthoized == .sharingAuthorized ? "\(Int(viewModel.healthStoreManager.latestHeartRate.value)) BPM" : "")
-          .font(.caption)
-          .foregroundColor(Color.black)
-      }
-      .padding(16)
+  var body: some View {
+    HStack {
+      Text(sectionTitle)
+        .font(.headline)
+        .foregroundColor(Color.primary)
+      Spacer()
     }
     .frame(height: 60)
     .frame(maxWidth: .infinity)
-    .background(Color.white)
-    .cornerRadius(8)
-    .shadow(radius: 12)
-    .onAppear {
-      viewModel.healthStoreManager.startObserveHeartRateSamples()
+  }
+}
+
+// MARK: Widget Card
+extension MainView {
+  // MARK: Summary Seciton
+  var heartRateWidgetCard: some View {
+    VStack(alignment: .leading, spacing: 8) {
+      HStack {
+        Image(systemName: "heart.fill")
+          .resizable()
+          .foregroundColor(viewModel.isHeartRateAuthoized == .sharingAuthorized ? .pink : .gray)
+          .frame(width: 12, height: 12)
+          .scaledToFit()
+        Text("Heart Rate")
+          .foregroundColor(viewModel.isHeartRateAuthoized == .sharingAuthorized ? .primary : .gray)
+          .font(.caption)
+          .fontWeight(.semibold)
+      }
+      .padding(16)
+      HStack(spacing: 8) {
+        Text(viewModel.isHeartRateAuthoized == .sharingAuthorized ? "\(Int(viewModel.healthStoreManager.latestHeartRate.value))" : "0")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(viewModel.isHeartRateAuthoized == .sharingAuthorized ? .pink : .gray)
+          .multilineTextAlignment(.center)
+        Text("BPM")
+          .foregroundColor(.primary)
+          .font(.caption2)
+          .multilineTextAlignment(.center)
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.bottom, 24)
     }
+    .frame(maxWidth: .infinity)
+    .background(Color(UIColor.systemBackground))
+    .cornerRadius(16)
     .onTapGesture {
       if viewModel.isHeartRateAuthoized != .sharingAuthorized {
         // lead user to setting page
@@ -107,64 +187,67 @@ extension MainView {
   }
   
   var zoneWidgetCard: some View {
-    VStack {
+    VStack(alignment: .leading, spacing: 8) {
       HStack {
         Image(systemName: "flame.fill")
+          .resizable()
           .foregroundColor(.pink)
-        Text("Zone").fontWeight(.medium).foregroundColor(.black)
-        Spacer()
-        Text("\(viewModel.healthStoreManager.currentZone?.zoneName ?? "")")
+          .frame(width: 12, height: 12)
+          .scaledToFit()
+        Text("Zone")
+          .foregroundColor(.primary)
           .font(.caption)
-          .foregroundColor(.black)
+          .fontWeight(.semibold)
       }
       .padding(16)
-    }
-    .frame(height: 60)
-    .frame(maxWidth: .infinity)
-    .background(Color.white)
-    .cornerRadius(8)
-    .shadow(radius: 12)
-  }
-  
-  var activitiesWidgetCard: some View {
-    VStack(spacing: 16) {
-      ForEach(viewModel.healthStoreManager.activities, id: \.self) { activity in
-        VStack {
-          HStack {
-            Image(systemName: "figure.indoor.cycle")
-              .foregroundColor(.pink)
-            Text("Indoor Cycling")
-              .font(.caption)
-              .foregroundColor(.black)
-            Spacer()
-          }
-          .padding(16)
-          .frame(height: 40)
-          
-          
-          HStack {
-            Text("Duration: xxxx s").font(.title2).foregroundColor(.black)
-          }
-        }
-        .frame(maxWidth: .infinity)
-        .background(Color.white)
-        .cornerRadius(8)
-        .shadow(radius: 12)
+      HStack(spacing: 8) {
+        Text("\(viewModel.healthStoreManager.currentZone?.zoneName ?? "Zone 1")")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(Color.pink)
+          .multilineTextAlignment(.center)
       }
+      .frame(maxWidth: .infinity)
+      .padding(.bottom, 24)
     }
+    .frame(maxWidth: .infinity)
+    .background(Color(UIColor.systemBackground))
+    .cornerRadius(16)
   }
 }
 
-struct MainViewSectionHeader: View {
-  let sectionTitle: String
+struct MainViewActivityCard: View {
+//  @State var activtiy: HKWorkout
+//
+//  init(activity: HKWorkout) {
+//    self.activtiy = activity
+//  }
   
   var body: some View {
-    HStack {
-      Text(sectionTitle)
-        .font(.headline)
+    HStack(spacing: 16) {
+      Image(systemName: "figure.indoor.cycle")
+        .resizable()
+        .scaledToFit()
+        .foregroundColor(Color(UIColor.systemPink))
+        .frame(maxWidth: 16)
+      VStack(alignment: .leading, spacing: 4) {
+        Text("Indoor Cycling")
+          .font(.caption)
+          .foregroundColor(.secondary)
+        Text("360.18 Kcal")
+          .fontWeight(.semibold)
+          .foregroundColor(.primary)
+        Text("30 mins")
+          .fontWeight(.semibold)
+          .foregroundColor(.primary)
+      }
+      
       Spacer()
+
     }
-    .frame(height: 60)
+    .padding(16)
     .frame(maxWidth: .infinity)
+    .background(Color(UIColor.systemBackground))
+    .cornerRadius(16)
   }
 }
