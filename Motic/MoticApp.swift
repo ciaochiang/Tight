08815b09e7bb0ed5832b9e49fcf4344f0056d295
Ciao Chiang <ciaochiang@gmail.com>
2023-09-18 18:47:46 +0800

    (feat)[#1] add firebase/crashlytics into project

--- Motic/MoticApp.swift
+++ Motic/MoticApp.swift
@@ -6,16 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct MoticApp: App {
-    var body: some Scene {
-        WindowGroup {
-          if AccountManager.shared.isLoggedIn {
-            MainView()
-          } else {
-            OnboardingView()
-          }
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      if AccountManager.shared.isLoggedIn {
+        MainView()
+      } else {
+        OnboardingView()
+      }
     }
+  }
 }
