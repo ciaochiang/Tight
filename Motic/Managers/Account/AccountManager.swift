e1f3a79a0df5650a6f81d602626e6ceaeb7c02f4
Ciao Chiang <ciaochiang@gmail.com>
2023-10-03 22:55:30 +0800

    (chore) modify logic process and fix location usage description missed issue

--- Motic/Managers/Account/AccountManager.swift
+++ Motic/Managers/Account/AccountManager.swift
@@ -9,66 +9,75 @@ import SwiftUI
 import AuthenticationServices
 
 class AccountManager: ObservableObject {
-  static let shared = AccountManager()
-  let logger: Logger = Logger(configuration: AppConfiguration.loggerConfig)
-  
-  @AppStorage("UserType") var userType: String?
-  @Published var currentUser: BaseUser?
-  @Published var isLoggedIn: Bool = false
-
-  init() {
-    switch userType {
-    case "apple":
-      // retrieve apple user data
-      isLoggedIn = true
-      retreiveCurrentUser()
-    default: break
-    }
-  }
-  
-  func retreiveCurrentUser() {
-    guard let appleUserData = UserDefaults.standard.data(forKey: "User"),
-          let appleUser = try? JSONDecoder().decode(AppleUser.self, from: appleUserData)
-    else { return }
+    static let shared = AccountManager()
+    let logger: Logger = Logger(configuration: AppConfiguration.loggerConfig)
     
-    currentUser = appleUser
-  }
+    @AppStorage("UserType") var userType: String?
+    @Published var currentUser: BaseUser?
+    @Published var isLoggedIn: Bool = false
+
+    init() {
+        // try login
+        if let userId = UserDefaults.standard.string(forKey: "UserId") {
+            login(userId: userId)
+        }
+    }
+    
+    @discardableResult func login(userId: String) -> Bool {
+        guard let userData = UserDefaults.standard.data(forKey: userId),
+              let user = try? JSONDecoder().decode(AppleUser.self, from: userData)
+        else { return false }
+        
+        currentUser = user
+        isLoggedIn = true
+        return true
+    }
   
-  func logout() {
-    // Clean user data
-    currentUser = nil
-    isLoggedIn = false
-  }
+    func retreiveCurrentUser() {
+        guard let appleUserData = UserDefaults.standard.data(forKey: "User"),
+              let appleUser = try? JSONDecoder().decode(AppleUser.self, from: appleUserData)
+        else { return }
+        
+        currentUser = appleUser
+    }
+    
+    func logout() {
+        // Clean user data
+        currentUser = nil
+        isLoggedIn = false
+    }
 }
 
 // MARK: SignInWithApple Functions
 extension AccountManager {
-  func requestAppleSignIn(_ request: ASAuthorizationAppleIDRequest) {
-    request.requestedScopes = [.fullName, .email]
-  }
-  
-  func handleAppleSignIn(_ authResult: Result<ASAuthorization, Error>) {
-    switch authResult {
-    case .success(let auth):
-      switch auth.credential {
-      case let appleIdCredential as ASAuthorizationAppleIDCredential:
-        if let appleUser = AppleUser(credential: appleIdCredential),
-            let appleUserData = try? JSONEncoder().encode(appleUser) {
-          UserDefaults.standard.setValue("apple", forKey: "UserType")
-          UserDefaults.standard.setValue(appleUserData, forKey: appleUser.userId)
-          logger.log("Save apple user: \(appleUser)", level: .info)
-        } else {
-          guard let appleUserData = UserDefaults.standard.data(forKey: "User"),
-                let appleUser = try? JSONDecoder().decode(AppleUser.self, from: appleUserData)
-          else { return }
-          
-          currentUser = appleUser
-        }
-      default:
-        logger.log("\(auth.credential)", level: .info)
-      }
-    case .failure(let error):
-      logger.log("Error: \(error.localizedDescription)", level: .error)
+    func requestAppleSignIn(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
     }
-  }
+    
+    func handleAppleSignIn(_ authResult: Result<ASAuthorization, Error>) {
+        switch authResult {
+        case .success(let auth):
+            switch auth.credential {
+            case let appleIdCredential as ASAuthorizationAppleIDCredential:
+                // try login
+                guard login(userId: appleIdCredential.user) == false else { return }
+                
+                // create user
+                guard let newUser = AppleUser(credential: appleIdCredential),
+                      let userData = try? JSONEncoder().encode(newUser) else { return }
+                
+                UserDefaults.standard.setValue("apple", forKey: "UserType")
+                UserDefaults.standard.setValue(newUser.userId, forKey: "UserId")
+                UserDefaults.standard.setValue(userData, forKey: newUser.userId)
+                logger.log("Save apple user: \(newUser)", level: .info)
+                
+                // login
+                currentUser = newUser
+            default:
+                logger.log("\(auth.credential)", level: .info)
+            }
+        case .failure(let error):
+            logger.log("Error: \(error.localizedDescription)", level: .error)
+        }
+    }
 }
