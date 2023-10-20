9ed14d882971933168a28bb2d91cc254c8cf6e21
Ciao Chiang <ciaochiang@gmail.com>
2023-10-20 23:02:24 +0800

    (feat)[watch] add tabview to watch app

--- MoticWatch Watch App/MoticWatchApp.swift
+++ MoticWatch Watch App/MoticWatchApp.swift
@@ -10,14 +10,18 @@ import SwiftUI
 @main
 struct MoticWatch_Watch_AppApp: App {
     private var logger = CustomLogger()
-    
+
     var body: some Scene {
         WindowGroup {
-            let connectivity = WatchConnectivityProvider(logger: logger)
-            let sportProvider = SportProvider(connectivity: connectivity)
-            let dependency = SportSelectorViewModelDependencyImp(logger: logger, sportProvider: sportProvider)
-            let viewModel = SportSelectorViewModel(dependency: dependency)
-            SportSelectorView(viewModel: viewModel, logger: logger)
+            TabView {
+                ContentView()
+                
+                let connectivity = WatchConnectivityProvider(logger: logger)
+                let sportProvider = SportProvider(connectivity: connectivity)
+                let dependency = SportSelectorViewModelDependencyImp(logger: logger, sportProvider: sportProvider)
+                let viewModel = SportSelectorViewModel(dependency: dependency)
+                SportSelectorView(viewModel: viewModel, logger: logger)
+            }
         }
     }
 }
