7a02c63c5edd73137e0bc6b24e898d655050ce0b
Ciao Chiang <ciaochiang@gmail.com>
2024-01-14 18:18:32 +0800

    (CHORE) change launch screen style and fastlane config

--- Tight/Scenes/Onboarding/OnboardingView.swift
+++ Tight/Scenes/Onboarding/OnboardingView.swift
@@ -6,15 +6,12 @@
 //
 
 import SwiftUI
-import AuthenticationServices
 
 struct OnboardingView: View {
     // Onboarding states:
     /*
      0 - Welcome screen
-     1 - Authorize healthkit
-     2 - Location
-     3 - Notification
+     1 - Notification
      2 - Done
      */
     @StateObject var viewModel: OnboardingViewModel
@@ -29,9 +26,7 @@ struct OnboardingView: View {
         VStack {
             TabView(selection: $viewModel.currentIndex) {
                 OnboadingWelcomView().tag(0)
-//                OnboardingHealthKitView().tag(1)
                 OnboardingNotificationView().tag(1)
-//                OnboardingLocationView().tag(3)
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             
@@ -47,24 +42,6 @@ struct OnboardingView: View {
             default: break
             }
         }
-      
-//      VStack {
-        // Content
-//        authorizeHealthKitSection
-        
-//        if viewModel.state == .welcome {
-//          SignInWithAppleButton(.continue,
-//                                onRequest: AccountManager.shared.requestAppleSignIn) { result in
-//            AccountManager.shared.handleAppleSignIn(result)
-//            self.handleNextButtonPressed()
-//          }
-//                                .signInWithAppleButtonStyle(.white)
-//                                .frame(height: 48)
-//                                .frame(maxWidth: .infinity)
-//                                .cornerRadius(8)
-//        }
-//      }
-//      .padding(30)
     }
 }
 
@@ -136,30 +113,6 @@ struct OnboadingWelcomView: View {
     }
 }
 
-//struct OnboardingHealthKitView: View {
-//    var body: some View {
-//        VStack(alignment: .leading, spacing: 8) {
-//            Text("connect")
-//                .font(.title)
-//                .fontWeight(.semibold)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .foregroundColor(Color.themeStyle.theme.secondaryTextColor)
-//            
-//            Text("Your Health")
-//                .font(.largeTitle)
-//                .fontWeight(.heavy)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .foregroundColor(Color.themeStyle.theme.accent)
-//            
-//            Text("We will save your workout data to the HealthKit to give you a consolidated view of your fitness activity and to help you track your progress over time")
-//                .font(.body)
-//                .foregroundColor(.themeStyle.theme.secondaryTextColor)
-//                .frame(maxWidth: 360, alignment: .leading)
-//        }
-//        .padding(32)
-//    }
-//}
-
 struct OnboardingNotificationView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
@@ -184,27 +137,3 @@ struct OnboardingNotificationView: View {
         .padding(32)
     }
 }
-
-//struct OnboardingLocationView: View {
-//    var body: some View {
-//        VStack(alignment: .leading, spacing: 8) {
-//            Text("record")
-//                .font(.title)
-//                .fontWeight(.semibold)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .foregroundColor(Color.themeStyle.theme.secondaryTextColor)
-//            
-//            Text("Your Paths")
-//                .font(.largeTitle)
-//                .fontWeight(.heavy)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//                .foregroundColor(Color.themeStyle.theme.accent)
-//            
-//            Text("To accurately track your outdoor workouts like running or cycling, we continuously monitor your location, even when the app is in the background.")
-//                .font(.body)
-//                .foregroundColor(.themeStyle.theme.secondaryTextColor)
-//                .frame(maxWidth: 360, alignment: .leading)
-//        }
-//        .padding(32)
-//    }
-//}
