54131c3b8553227f0ccee315e4093d3100de3621
Ciao Chiang <ciaochiang@gmail.com>
2024-01-27 13:50:04 +0800

    (FEAT) add exteneded runtime session capability to watch app

--- TightWatch Watch App/TightWatchApp.swift
+++ TightWatch Watch App/TightWatchApp.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 @main
 struct TightWatch_Watch_AppApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var viewModel: ContentViewModel = ContentViewModel()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase, { oldValue, newValue in
+            switch newValue {
+            case .active:
+                // App became active
+                print("App is active")
+            case .inactive:
+                // App became inactive
+                print("App is inactive")
+            case .background:
+                // App is in the background
+                print("App is in the background")
+            @unknown default:
+                // Handle any future lifecycle phases
+                break
+            }
+        })
         .environmentObject(viewModel)
     }
 }
