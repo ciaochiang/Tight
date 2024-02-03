f67f47f1b79bd526aa4f29ef778d4f5277b5d3ab
Ciao Chiang <ciaochiang@gmail.com>
2024-02-03 16:10:38 +0800

    (FIX) training session manager crarsh when interact with watch

--- Tight/Managers/Training/TrainingSessionManager+Watch.swift
+++ Tight/Managers/Training/TrainingSessionManager+Watch.swift
@@ -26,4 +26,23 @@ extension TrainingSessionManager {
                                         .totalProgress: content.totalProgress
                                        ])
     }
+    
+    func onUpdateWatch(content: TrainingSessionContent) {
+        guard let exercise = content.currentExercise, let startTime = content.startTime else { return }
+                
+        /// Send message to Watch
+        TTWCSession.shared.sendMessage([.state: content.state.rawValue,
+                                        .exerciseID: exercise.id.uuidString,
+                                        .exerciseName: exercise.exercise.name,
+                                        .startTime: startTime,
+                                        .weight: exercise.weight,
+                                        .weightUnit: exercise.weightUnit,
+                                        .repetitions: exercise.repetitions,
+                                        .restStartTime: content.restStartTime ?? .now,
+                                        .restInterval: content.restInterval,
+                                        .indexOfSet: content.indexOfSet,
+                                        .setsProgress: content.setsProgress,
+                                        .totalProgress: content.totalProgress
+                                       ])
+    }
 }
